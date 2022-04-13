3c2636439294d2bc2310b96dd4b0ae0fce13fe7b
Yannick Béot <yannick.beot@sailpoint.com>
2022-04-13 11:20:24 +0200

    use of organization icon instead of sailpoint

--- src/models/IdentityNowTreeItem.ts
+++ src/models/IdentityNowTreeItem.ts
@@ -13,13 +13,9 @@ export class TenantTreeItem extends TreeItem {
         context: ExtensionContext
     ) {
         super(tenantName, TreeItemCollapsibleState.Collapsed);
-        this.iconPath = {
-            light: context.asAbsolutePath('resources/sailpoint.svg'),
-            dark: context.asAbsolutePath('resources/dark/sailpoint.svg')
-        };
     }
+    iconPath = new ThemeIcon('organization');
     contextValue = 'tenant';
-
 }
 
 /**
