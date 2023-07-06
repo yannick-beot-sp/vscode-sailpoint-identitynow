487f41f53abfa2b516df2ca3048014900c722f99
Richard Cook <Richard.Cook@asggroup.com.au>
2023-07-06 15:15:16 +0800

    Added support for updating AP's and Roles

--- src/files/IdentityNowResourceProvider.ts
+++ src/files/IdentityNowResourceProvider.ts
@@ -136,7 +136,7 @@ export class IdentityNowResourceProvider implements FileSystemProvider {
 				data = JSON.stringify(transform);
 			}
 
-			if (resourcePath.match("identity-profiles")) {
+			if (resourcePath.match("identity-profiles") || resourcePath.match("access-profiles") || resourcePath.match("roles")) {
 				// match identity profiles & lifecycle states
 				const oldData = await client.getResource(resourcePath);
 				const newData = JSON.parse(data);
@@ -156,12 +156,12 @@ export class IdentityNowResourceProvider implements FileSystemProvider {
 					patchResourcePath,
 					JSON.stringify(jsonpatch)
 				);
-			} else if (resourcePath.match("access-profiles")) {
-				// Currently not supported
-				throw vscode.FileSystemError.Unavailable('Modification of Access Profiles is not yet available!');
-			} else if (resourcePath.match("roles")) {
-				// Currently not supported
-				throw vscode.FileSystemError.Unavailable('Modification of Roles is not yet available!');
+			// } else if (resourcePath.match("access-profiles")) {
+			// 	// Currently not supported
+			// 	throw vscode.FileSystemError.Unavailable('Modification of Access Profiles is not yet available!');
+			// } else if (resourcePath.match("roles")) {
+			// 	// Currently not supported
+			// 	throw vscode.FileSystemError.Unavailable('Modification of Roles is not yet available!');
 			} else {
 				const updatedData = await client.updateResource(resourcePath, data);
 				if (!updatedData) {
